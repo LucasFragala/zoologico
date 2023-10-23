d30caaa971c7ec7430e50630b5a6178ce6fc4c82
Lucas Fragalá <lucfragala@gmail.com>
2023-10-23 12:12:51 -0300

    up

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class HelloController
+class HelloController extends AbstractController
 {
     #[Route('/')]
     public function index(): Response
     {
-        return new Response('Olá Mundo!');
+        $users = [
+        ['name' => 'Cachorro', 'idade'=> '12'],
+        ['name' => 'Gato', 'idade'=> '8'],
+        ['name' => 'Passaro', 'idade'=> '13'],
+        ['name' => 'Coelho', 'idade'=> '4'],
+        ['name' => 'Hammster', 'idade'=> '2'],
+        ['name' => 'Cavalo', 'idade'=> '10'],
+        ];
+
+        return $this->render('hello/homepage.html.twig',[
+            'title'=> 'Home',
+            'users' => $users,
+        ]);
     }
 
     #[Route('/animal/{slug}')]
